409aec337679d764ae349bdd42889904ea51e338
ryan fitzgerald <ryanfitz73@gmail.com>
2025-04-26 17:06:47 +0400

    rails task manager - Models and CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :tasks
+  # resources :tasks
+  Rails.application.routes.draw do
+    resources :tasks, only: [:index, :show]
+  end
 end
